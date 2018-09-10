14a13becf6958532cd807a400a52e4a647622d87
Ali Sufyan <alisufyan327@gmail.com>
2018-09-10 09:21:17 +0500

    Add custom user-agent and disable Safe Browsing

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ url = args.url;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3508.0 Safari/537.36')
     await page
         .goto(url, {
-            waitUntil: "load"
+            waitUntil: "load",
+            args: ["--disable-client-side-phishing-detection", "--safebrowsing-disable-download-protection", "--safebrowsing-manual-download-blacklist"]
         })
         .catch(e => (error = e));
     await page.screenshot({ path: "screenshot.png", fullPage: true });
